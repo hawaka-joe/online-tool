665512d2e947396ac645d398a25f01f5b3d213e8
ZHOU HUIWEI <you@example.com>
2025-07-08 00:43:31 +0800

    添加静态文件服务器

--- src/app.cjs
+++ src/app.cjs
@@ -1,12 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 const excelRouter = require('./router/excelImage.cjs');
+const path = require('path');
 
 const port = 80;
 
 const app = express();
 
 app.use(cors());
+
+app.use(express.static(path.join(__dirname, '../frontend/src'))); // 假设静态资源放在 public 目录
 
 // 启动服务
 app.listen(port, () => {
